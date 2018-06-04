18ce6919e728b70d0f51550696cc5a0080e85108
Thijs <thetcr@live.nl>
2018-06-04 14:44:47 +0200

    Changes for the WISPR network.

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x20, 0x45, 0x12, 0x77 };
 
 CMessageHeader::CMessageHeader()
 {
